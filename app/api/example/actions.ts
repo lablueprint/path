b3b20c65e4b55ddc90bd56a42ff01067d8612abe
joy-y-cheng <joycheng@ucla.edu>
2025-11-17 20:21:53 -0800

    Move lib directory

--- app/api/example/actions.ts
+++ app/api/example/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { ExampleType } from "@/app/types/example";
-import { createClient } from "@/lib/supabase/server-client";
+import { createClient } from "@/app/lib/supabase/server-client";
 
 export const createExampleEntry = async (data: ExampleType) => {
   const supabase = await createClient();
