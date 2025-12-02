80a12a23ca2c5b8e88b98f69065b144bc178fd7a
Raymond Kao <rmykao@g.ucla.edu>
2025-12-01 23:04:46 -0800

    ws 5
    Co-authored-by: Neelesh Seerapu <neeleshseerapu@users.noreply.github.com>

--- app/(pages)/example/page.tsx
+++ app/(pages)/example/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { createExampleEntry } from "@/app/api/example/actions";
+import { createTicket } from "@/app/api/tickets/actions"
 
 export default function Example() {
   const data = {
@@ -8,9 +9,19 @@ export default function Example() {
     name: 'harry',
   };
 
+  const ticketData = {
+    requestor_user_id: "4c4f3502-1b31-4040-8a7a-2baedbc8a347",
+    store_id: "94b6329f-7383-46e2-978d-e105d31c3813",
+    status: "Pending",
+  }
+
   const handleClick = async () => {
     await createExampleEntry(data);
   };
+
+  const submitTicket = async () => {
+    await createTicket(ticketData);
+  }
 
   return (
     <div>
@@ -18,6 +29,10 @@ export default function Example() {
       <br />
       <button onClick={handleClick}>
         Click to add data to the example table
+      </button>
+      <br />
+      <button onClick={submitTicket}>
+        Click to add ticket to the ticket table
       </button>
     </div>
   );
--- app/api/tickets/actions.ts
+++ app/api/tickets/actions.ts
@@ -1,9 +1,34 @@
 "use server"; // directive that indicates that the functions we define are server actions
 
+import { Ticket } from "@/app/types/Ticket";
 import { createClient } from "@/app/lib/supabase/server-client";
 
-/*export async function createTicket(formData) {
+export async function createTicket(formData: Ticket) {
     const supabase = await createClient();
     // pulling out the error from the insert into tickets table
-    const { error: err } = await supabase.from('tickets').insert({ticket_id: })
-}*/
+    const { error: err } = await supabase.from("tickets").insert({
+        requestor_user_id: formData.requestor_user_id,
+        store_id: formData.store_id,
+        status: formData.status,
+    });
+
+    if (err) {
+        return { success: false, error: err };
+    }
+    return { success: true };
+}
+
+export async function deleteTickets(ticketId: string) {
+    const supabase = await createClient();
+    const { error: err } = await supabase.from("tickets").delete().eq("ticket_id", ticketId);
+    if (err) {
+        return { success: false, error: err };
+    }
+    return { success: true };
+}
+
+export async function updateTicketStatus(status: string) {
+    const supabase = await createClient();
+    const { error: err } = await supabase.from("tickets")
+    //finish and check doc for the 4 status can also make enum 
+}
