a47249430b7f9971f9ca4e1bd5e1d6bcd59a1d63
cheyennelu17 <cheyennelu17@g.ucla.edu>
2025-12-18 09:23:22 +0800

    add roles schema

---
+++ supabase/schemas/roles.sql
@@ -0,0 +1,22 @@
+create table roles 
+(
+    role_id INT not null  primary key,
+    name TEXT not null
+);
+
+alter table "roles" enable row level security;
+
+create policy "authenticated users can select entries"
+on public.roles
+for select to authenticated
+using (true);
+
+create policy "no users can insert entries"
+on public.roles
+for insert
+with check (false);
+
+create policy "no user can delete entries"
+on public.roles
+for delete
+using (false);
