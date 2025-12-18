00a2f2f2c6e18d8322f4d7ad3611f0acacfade15
cheyennelu17 <cheyennelu17@g.ucla.edu>
2025-12-18 12:21:33 +0800

    correct RLS for roles.sql

--- supabase/schemas/roles.sql
+++ supabase/schemas/roles.sql
@@ -11,9 +11,16 @@ on public.roles
 for select to authenticated
 using (true);
 
+
 create policy "no users can insert entries"
 on public.roles
 for insert
+with check (false);
+
+create policy "no users can update entries"
+on public.roles
+for update
+using (false);
 with check (false);
 
 create policy "no user can delete entries"
