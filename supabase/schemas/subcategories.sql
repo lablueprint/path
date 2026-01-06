f2c971b3fd73d6f885f2362c40e82aca598b6b9c
1004yeeun <joycejeoung@ucla.edu>
2026-01-05 21:27:01 -0800

    add rls policies for subcategories table

--- supabase/schemas/subcategories.sql
+++ supabase/schemas/subcategories.sql
@@ -14,3 +14,25 @@ create table "subcategories" (
   constraint uq_category_id_name
     unique (category_id, name)
 );
+
+alter table subcategories enable row level security;
+
+create policy "auth can read subcategories if >= requestor"
+on public.subcategories for select
+to authenticated
+using ((select auth.jwt()) ->> 'user_role' in ('requestor', 'admin', 'superadmin', 'owner'));
+
+create policy "auth can insert subcategories if >= admin"
+on public.subcategories for insert
+to authenticated
+using ((select auth.jwt()) ->> 'user_role' in ('admin', 'superadmin', 'owner'));
+
+create policy "auth can update subcategories if >= superadmin"
+on public.subcategories for update
+to authenticated
+using ((select auth.jwt()) ->> 'user_role' in ('superadmin', 'owner'));
+
+create policy "auth can delete subcategories if >= superadmin"
+on public.subcategories for delete
+to authenticated
+using ((select auth.jwt()) ->> 'user_role' in ('superadmin', 'owner'));
