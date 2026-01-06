6a1873653559360145d643628cfc3a8ba589838f
whsieh12 <williamoda@g.ucla.edu>
2026-01-05 22:14:13 -0800

    update function

--- supabase/schemas/handle_new_user.sql
+++ supabase/schemas/handle_new_user.sql
@@ -1,17 +1,28 @@
 create schema if not exists private;
 
-create function private.handle_new_user()
+create or replace function private.handle_new_user()
 returns trigger
 language plpgsql
-security definer set search_path = ''
+security definer
+set search_path = ''
 as $$
 begin
-    insert into users (user_id, first_name, last_name)
-    values (new.id, new.raw_user_meta_data ->> 'first_name', new.raw_user_meta_data ->> 'last_name');
-    return new;
+
+  insert into public.users (user_id, first_name, last_name, email)
+  values (
+    new.id,
+    new.raw_user_meta_data ->> 'first_name',
+    new.raw_user_meta_data ->> 'last_name',
+    new.email
+  );
+
+  return new;
 end;
 $$;
 
-create trigger on_auth_user_created
-    after insert on auth.users
-    for each row execute procedure private.handle_new_user();
+drop trigger if exists "after create auth.users" on auth.users;
+
+create trigger "after create auth.users"
+after insert on auth.users
+for each row
+execute procedure private.handle_new_user();
