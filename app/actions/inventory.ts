e6d3d49488ac87440f05e755b93af71c0d074a64
1004yeeun <joycejeoung@ucla.edu>
2026-01-05 22:17:55 -0800

    implemented server actions for subcategories

--- app/actions/inventory.ts
+++ app/actions/inventory.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { InventoryItem, InventoryItemInsert } from '@/app/types/inventory';
+import { InventoryItem, InventoryItemInsert, Subcategory, SubcategoryInsert, SubcategoryUpdate } from '@/app/types/inventory';
 import { createClient } from '@/app/lib/supabase/server-client';
 
 export const createItem = async (data: InventoryItemInsert) => {
@@ -46,6 +46,54 @@ export const deleteItem = async (inventoryItemId: string) => {
 
   if (err) {
     console.error('Error deleting inventory item:', err);
+    return { success: false, error: err.message };
+  }
+  return { success: true };
+};
+
+export const createSubcategory = async (data: SubcategoryInsert) => {
+  const supabase = await createClient();
+  const { data: entry, error: err } = await supabase
+    .from('subcategories')
+    .insert(data)
+    .select()
+    .single();
+
+  if (err) {
+    console.error('Error creating subcategory:', err);
+    return { success: false, data: null, error: err.message };
+  }
+  return { success: true, data: entry as Subcategory };
+};
+
+export const updateSubcategory = async (
+  subcategoryId: string,
+  data: SubcategoryUpdate,
+) => {
+  const supabase = await createClient();
+  const { data: entry, error: err } = await supabase
+    .from('subcategories')
+    .update(data)
+    .eq('subcategory_id', subcategoryId)
+    .select()
+    .single();
+
+  if (err) {
+    console.error('Error updating subcategory:', err);
+    return { success: false, data: null, error: err.message };
+  }
+  return { success: true, data: entry as Subcategory };
+};
+
+export const deleteSubcategory = async (subcategoryId: string) => {
+  const supabase = await createClient();
+  const { error: err } = await supabase
+    .from('subcategories')
+    .delete()
+    .eq('subcategory_id', subcategoryId);
+
+  if (err) {
+    console.error('Error deleting subcategory:', err);
     return { success: false, error: err.message };
   }
   return { success: true };
