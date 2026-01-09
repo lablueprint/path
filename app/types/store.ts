87accb9fac45cf2c15ed4edc19780ab3d35c8a69
Tara Kaviani <tarakaviani@g.ucla.edu>
2026-01-08 21:11:20 -0800

    add types in store.ts

--- app/types/store.ts
+++ app/types/store.ts
@@ -7,3 +7,25 @@ export type Store = {
 export type StoreInsert = Omit<Store, 'store_id'>;
 
 export type StoreUpdate = Partial<StoreInsert>;
+
+export type StoreAdmin = {
+  store_admins_id: string;
+  user_id: string;
+  store_id: string;
+};
+
+export type StoreAdminInsert = Omit<StoreAdmin, 'store_admins_id'>;
+
+export type StoreItem = {
+  store_item_id: string;
+  store_id: string;
+  inventory_item_id: string;
+  quantity: number;
+};
+
+export type StoreItemInsert = Omit<StoreItem, 'store_item_id'>;
+
+
+
+
+
