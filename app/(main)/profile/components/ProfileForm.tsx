3b886c4d44b1c0ca58e243550a08d004aa8a4344
1004yeeun <joycejeoung@ucla.edu>
2026-01-08 22:11:48 -0800

    installed react-hook-form and scaffolded profile page + ProfileForm component

---
+++ app/(main)/profile/components/ProfileForm.tsx
@@ -0,0 +1,7 @@
+import type { User } from '@/app/types/user';
+
+export default function ProfileForm( {user} : {user: User} ) {
+  return (
+    <div>This is the profile form.</div>
+  );
+}
---
+++ app/(main)/profile/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import ProfileForm from './components/ProfileForm';
+import type { User } from '@/app/types/user';
+
+export default function PersonalProfilePage() {
+    const user: User = {
+        user_id: 'example_user_id',
+        first_name: 'example_first',
+        last_name: 'example_last',
+        email: 'example@example.com',
+        profile_photo_url: 'https://example.com/photo.jpg'
+    };
+
+    return (
+        <div>
+            This is a Profile Page.
+            <br />
+            <ProfileForm user={user} />
+        </div>
+    );
+}
