a570157a6522a4de33b441c4677cb747cf18efa0
minado05 <athudo@g.ucla.edu>
2026-01-08 22:50:02 -0800

    Added route protection

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { createClient } from '@/app/lib/supabase/browser-client';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type FormInput = {
+  email: string;
+  password: string;
+};
+
+export default function SignInPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>();
+  const supabase = createClient();
+
+  const onSubmit = async (Input: FormInput) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: Input.email,
+      password: Input.password,
+    });
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register('email', {
+          pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          required: true,
+        })}
+        placeholder="Email"
+      />
+      {errors.email?.type === 'required' && (
+        <p role="alert"> Email is required </p>
+      )}
+      {errors.email?.type === 'pattern' && <p role="alert">Invalid Email</p>}
+      <input
+        {...register('password', { required: true })}
+        placeholder="Password"
+        type="password"
+      />
+
+      {errors.password?.type === 'required' && (
+        <p role="alert"> Password is required </p>
+      )}
+      <button type="submit">Sign In</button>
+      <a href="/sign-up">Sign Up</a>
+    </form>
+  );
+}
--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,5 +1,16 @@
 'use client';
+import { createClient } from '@/app/lib/supabase/browser-client';
 
 export default function HomePage() {
-  return <div>Welcome to PATH! This is the home page.</div>;
+  const supabase = createClient();
+  async function SignOut() {
+    const { error } = await supabase.auth.signOut();
+  }
+
+  return (
+    <div>
+      <div>Welcome to PATH! This is the home page.</div>
+      <button onClick={SignOut}>Sign Out</button>
+    </div>
+  );
 }
