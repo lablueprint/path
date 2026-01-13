05cbc8bbc23eb9b21bd2d6ea4c5727eea2b498e5
1004yeeun <joycejeoung@ucla.edu>
2026-01-12 21:22:58 -0800

    Save button calls updateUser when clicked

--- app/(main)/profile/components/ProfileForm.tsx
+++ app/(main)/profile/components/ProfileForm.tsx
@@ -1,17 +1,27 @@
 import type { User } from '@/app/types/user';
 import { useForm } from 'react-hook-form';
+import { updateUser } from '@/app/actions/user'
 
 export default function ProfileForm( {user} : {user: User} ) {
-    const { register } = useForm({
+    const { register, handleSubmit } = useForm({
         defaultValues: {
             first_name: user.first_name ?? 'unknown_first_name',
             last_name: user.last_name ?? 'unknown_last_name',
         },
     });
 
+    const onSubmit = (data: { first_name: string; last_name: string }) => {
+        updateUser(user.user_id, {
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: user.email,
+            profile_photo_url: user.profile_photo_url,
+        });
+    };
+
     return (
         <div>This is the profile form.
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <br />
                 {/* First name */}
                 <div>
@@ -26,11 +36,7 @@ export default function ProfileForm( {user} : {user: User} ) {
                 </div>
                 <br />
                 {/* save button */}
-                <button
-                    type='submit'
-                >
-                    Save
-                </button>
+                <button type='submit'>Save</button>
             </form>
         </div>
     );
