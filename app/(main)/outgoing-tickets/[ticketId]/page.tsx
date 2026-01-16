75607b8f7029806539a4494afacb0eeee2d3310a
Camillalalala <camilla4clubs@gmail.com>
2026-01-15 22:32:54 -0800

    Created fetch ticket and fetch store functions. Incomplete

---
+++ app/(main)/outgoing-tickets/[ticketId]/page.tsx
@@ -0,0 +1,44 @@
+'use server';
+import { createClient } from '@/app/lib/supabase/server-client';
+
+interface OutGoingTicketsDetailsPageProps{
+    params: {
+        ticketId : string;
+    };
+}
+
+async function fetchTicket(id: string) {
+    const supabase = await createClient();
+    const { data, error } = await supabase
+        .from('tickets')
+        .select('*')
+        .eq('id', id)
+        .single();
+    if (error) throw error;
+    return data;
+}
+
+
+aysnc function fetchStore(storeId: string) {
+    const supabase = createClient
+}
+export default async function OutGoingTicketsDetailsPage({
+    params
+}: OutGoingTicketsDetailsPageProps) {
+    const { ticketId } = await params;
+    //1. Fetch Ticket
+    const ticket = await fetchTicket(ticketId)
+    if(!ticketId){
+        throw new Error('Ticket not found');
+    }
+
+    //2. Fetch store using ticket object
+    const store = await fetchStore(ticket.store_id);
+    if(!ticket.store_id){
+        throw new Error('Store not found')
+    }
+
+    return (
+        <div>{ticket.status}</div>
+    );
+}
