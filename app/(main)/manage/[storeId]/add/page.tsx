4e9f9e3e2037d64ac7f5b80b062232490adb600b
katieyungchung <katieyungchung@g.ucla.edu>
2026-01-20 22:13:39 -0800

    remove params from page.tsx

--- app/(main)/manage/[storeId]/add/page.tsx
+++ app/(main)/manage/[storeId]/add/page.tsx
@@ -1,33 +1,9 @@
-import React from "react";
 import DonationForm from "./components/DonationForm";
 
-interface PageProps {
-  params: { id: string }; // Next.js dynamic route param
-}
-
-export default function AddStoreItemsPage({ params }: PageProps) {
-  const { id } = params;
-
+export default function AddStoreItemsPage() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        padding: "2rem",
-      }}
-    >
-  
-      <div
-        style={{
-          display: "flex",
-          alignItems: "flex-start",
-        }}
-      >
-
-        <div style={{ flex: 1 }}>
-          
-          <DonationForm />
-        </div>
-      </div>
+    <div>
+      <DonationForm />
     </div>
   );
 }
