5e7db398aaf036afec5916ae3d271abdcfac03e2
Raymond Kao <rmykao@gmail.com>
2026-01-29 23:02:23 -0800

    updated quantity button

--- app/components/InStockTicketItemCard.tsx
+++ app/components/InStockTicketItemCard.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { useState } from "react";
+import { updateTicketItemQuantity } from "../actions/ticket";
+import { set } from "react-hook-form";
+
 interface InStockTicketItemCardProps {
   ticketItemId: string;
   quantityRequested: number;
@@ -19,14 +23,38 @@ export default function InStockTicketItemCard({
   subcategoryName,
   categoryName,
 }: InStockTicketItemCardProps) {
+  const [quantity, setQuantity] = useState(quantityRequested);
+  const [savedQuantity, setSavedQuantity] = useState(quantityRequested);  // NEW
+  const hasChanged = quantity !== savedQuantity;
+
+  const handleSave = async () => {
+    await updateTicketItemQuantity(ticketItemId, quantity);
+    setSavedQuantity(quantity);
+  };
+  const handleCancel = () => {
+    setQuantity(savedQuantity);
+  };
+
   return (
+    
     <div className="border p-4 rounded-lg">
-      <img src={photoUrl} alt={itemName} className="w-32 h-32 object-cover" />
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+       />
+      {hasChanged && (
+        <>
+        <button onClick={handleSave}>Save</button>
+        <button onClick={handleCancel}>Cancel</button>
+        </>
+      )}
       <h3>{itemName}</h3>
       <p>Category: {categoryName}</p>
       <p>Subcategory: {subcategoryName}</p>
       <p>Quantity Requested: {quantityRequested}</p>
       <p>Available: {quantityAvailable}</p>
+      <button></button>
     </div>
   );
 }
