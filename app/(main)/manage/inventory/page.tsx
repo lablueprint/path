0980a8492c83dbc99f6a23d1b0ad910041dc3790
cheyennelu17 <cheyennelu17@g.ucla.edu>
2026-02-12 22:58:41 -0800

    correcting merge conflicts

--- app/(main)/manage/inventory/page.tsx
+++ app/(main)/manage/inventory/page.tsx
@@ -1,47 +1,48 @@
 import InventoryItemCard from '@/app/(main)/manage/inventory/components/InventoryItemCard';
 import { createClient } from '@/app/lib/supabase/server-client';
 export default async function InventoryPage() {
-    const supabase = await createClient();
-    const { data: items, error: itemError } = await supabase
-        .from('inventory_items')
-        .select(
-            `inventory_item_id, name, photo_url, subcategories(name, categories(name))`,
-        );
-    if (itemError) {
-        console.error(itemError);
-        return <div>Error loading inventory items</div>;
-    }
+  const supabase = await createClient();
+  const { data: items, error: itemError } = await supabase
+    .from('inventory_items')
+    .select(
+      `inventory_item_id, name, photo_url, subcategories(name, categories(name))`,
+    );
+  if (itemError) {
+    console.error(itemError);
+    return <div>Error loading inventory items</div>;
+  }
 
-    const { data: categories, error } = await supabase.from('categories').select(`category_id, name, photo_url, subcategories(subcategory_id, name)`).order("name", { ascending: true }).order("name", { foreignTable: "subcategories", ascending: true })
-    if (error) {
-        console.error(error)
-        return <div>Error loading inventory items</div>
-    }
+  const { data: categories, error } = await supabase
+    .from('categories')
+    .select(`category_id, name, subcategories(subcategory_id, name)`)
+    .order('name', { ascending: true })
+    .order('name', { foreignTable: 'subcategories', ascending: true });
+  if (error) {
+    console.error(error);
+    return <div>Error loading categories</div>;
+  }
 
-    return <div>
-        <div>
-            {items?.map((item) => (
-                <InventoryItemCard
-                    key={item.inventory_item_id}
-                    item={item}
-                />
-            ))}
-        </div>
-        <div>
-            {categories?.map((cat) => (
-                <div key={cat.category_id}>
-                    <h2>{cat.name}</h2>
-                    <ul>
-                        {cat.subcategories?.map((sub) => (
-                            <li key={sub.subcategory_id}>
-                                {sub.name}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
-        </div>
+  return (
+    <div>
+      <div>
+        {items?.map((item) => (
+          <div>
+            <InventoryItemCard key={item.inventory_item_id} item={item} />
+          </div>
+        ))}
+      </div>
+      <div>
+        {categories?.map((cat) => (
+          <div key={cat.category_id}>
+            <h2>{cat.name}</h2>
+            <ul>
+              {cat.subcategories?.map((sub) => (
+                <li key={sub.subcategory_id}>{sub.name}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
-
-
+  );
 }
