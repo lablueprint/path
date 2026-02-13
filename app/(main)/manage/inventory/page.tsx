b56208960b2de2e5d005e3a1def92552e0ff1104
minado05 <athudo@g.ucla.edu>
2026-02-12 22:09:58 -0800

    added inventory page component

--- app/(main)/manage/inventory/page.tsx
+++ app/(main)/manage/inventory/page.tsx
@@ -1,7 +1,42 @@
+import { createClient } from "@/app/lib/supabase/server-client";
 export default async function InventoryPage() {
     const supabase = await createClient();
-    const { data, error } = await supabase.from('inventory_items').select(`inventory_item_id, name, photo_url, subcategories(name), categories(name)`)
+    const { data: items, error: itemError } = await supabase.from('inventory_items').select(`inventory_item_id, name, photo_url, subcategories(name), categories(name)`)
+    if (itemError) {
+        console.error(itemError)
+        return <div>Error loading inventory items</div>
+    }
 
+    const { data: categories, error } = await supabase.from('categories').select(`category_id, name, photo_url, subcategories(name), subcategories(subcategory_id, name)`).order("name", { ascending: true }).order("name", { foreignTable: "subcategories", ascending: true })
+    if (error) {
+        console.error(error)
+        return <div>Error loading inventory items</div>
+    }
 
-    return <div />
+    return <div>
+        <div>
+            {items?.map((item) => (
+                <InventoryItemCard
+                    key={item.inventory_item_id}
+                    item={item}
+                />
+            ))}
+        </div>
+        <div>
+            {categories?.map((cat) => (
+                <div key={cat.category_id}>
+                    <h2>{cat.name}</h2>
+                    <ul>
+                        {cat.subcategories?.map((sub) => (
+                            <li key={sub.subcategory_id}>
+                                {sub.name}
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
+            ))}
+        </div>
+
+    </div>
 }
