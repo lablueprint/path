0417d3a9dcfc05099e1bf6279e1e16126d61199a
1004yeeun <joycejeoung@ucla.edu>
2026-02-17 18:16:01 -0800

    add ManageStoreItemCard on manage store page with item info + link items to detail page

--- app/(main)/manage/[storeId]/page.tsx
+++ app/(main)/manage/[storeId]/page.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@/app/lib/supabase/server-client';
+import ManageStoreItemCard from '../components/ManageStoreItemCard';
 
 export default async function ManageStorePage({
   params,
@@ -24,16 +25,13 @@ export default async function ManageStorePage({
     .select(
       `
             store_item_id,
-            inventory_item_id,
             item_name:inventory_item_id(name),
             item_photo_url:inventory_item_id(photo_url),
-            subcategory_id:inventory_items(subcategory_id),
             subcategory_name:inventory_items(subcategories(name)),
             category_name:inventory_items(subcategories(categories(name)))
         `,
     )
     .eq('store_id', storeId);
-  console.log('Hello', items);
   if (items_error) {
     console.error('Error fetching store items:', items_error);
     return <div>Failed to load store items.</div>;
@@ -41,50 +39,25 @@ export default async function ManageStorePage({
 
   return (
     <div>
-      <h1>Store Info</h1>
-      <h3>Store ID: {storeId}</h3>
-      <h3>Store Name: {store.name}</h3>
-      <h3>Store Address: {store.street_address}</h3>
+      {/* store info */}
+      <div>
+        <h1>Store Info</h1>
+        <h3>Store ID: {storeId}</h3>
+        <h3>Store Name: {store.name}</h3>
+        <h3>Store Address: {store.street_address}</h3>
+      </div>
+
+      {/* store items + info */}
+      <div>
+        <h1>Store Items:</h1>
+        {(items ?? []).map((item) => (
+          <ManageStoreItemCard 
+            key={item.store_item_id} 
+            item={item} 
+            storeId={storeId}
+          />
+        ))}
+      </div>
     </div>
   );
-
-  // // const { data: store_item_data, error: store_item_err } = await supabase
-  // //     .from('store_items')
-  // //     .select(`
-  // //         inventory_item_id,
-  // //         item_name:inventory_item_id(name),
-  // //         item_photo_url:inventory_item_id(photo_url),
-  // //         item_subcat_name:inventory_item_id(subcategory_id(name)),
-  // //         item_cat_name:inventory_item_id(subcategory_id(category_id(name))),
-  // //     `)
-  // //     .eq('store_id', storeId);
-  // const { data: store_item_data, error: store_item_err } = await supabase
-  //     .from('store_items')
-  //     .select(`
-  //         inventory_item_id,
-  //         inventory_items(name)
-  //     `)
-  //     .eq('store_id', storeId);
-  // console.log("hello")
-  // console.log(store_item_data)
-  // // only noted in console, rest of the page continues
-  // if (store_item_err) {
-  //     console.log('Error fetching store items:', store_item_err);
-  // }
-
-  // return (
-  //     <div>
-  //         <h1>Store Info</h1>
-  //         <h3>Name: {store_data.name}</h3>
-  //         <h3>Address: {store_data.street_address}</h3>
-  //         <h3>Item:</h3>
-  //         {store_item_data?.length ? (
-  //             <ul>
-  //                 {store_item_data.map((item) => (
-  //                     <li>{item.inventory_item_id}: {(item.inventory_items as any)?.name}</li>
-  //                 ))}
-  //             </ul>
-  //         ) : (<p>No items found for this store.</p>)}
-  //     </div>
-  // );
 }
