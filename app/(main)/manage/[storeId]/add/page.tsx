e59f2238cc46bda4f7c13e92dbc12d11d84d447d
minado05 <athudo@g.ucla.edu>
2026-02-19 22:16:09 -0800

    modified add/page

--- app/(main)/manage/[storeId]/add/page.tsx
+++ app/(main)/manage/[storeId]/add/page.tsx
@@ -1,6 +1,13 @@
+'use server';
 import DonationForm from './components/DonationForm';
+import { createClient } from '@/app/lib/supabase/server-client';
 
-export default function AddStoreItemsPage() {
+export default async function AddStoreItemsPage({ params, }: { params: Promise<{ storeId: string }> }) {
+  const { storeId } = await params
+  const supabase = await createClient()
+  const { data, error } = await supabase.from('stores').select('*').eq('store_id', storeId).single();
+  const { data: { user } } = await supabase.auth.getUser()
+
   return (
     <div>
       <DonationForm />
