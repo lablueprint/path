4a79fd2d7edf99d7ae035f10608d870d99dc1f76
neelesh <neeleshseerapu@gmail.com>
2026-02-19 23:11:54 -0800

    pre modifying outgoing ticket details

--- app/(main)/components/TicketDetails.tsx
+++ app/(main)/components/TicketDetails.tsx
@@ -1,3 +1,172 @@
-export default function TicketDetails({ ticketId, outgoing }: { ticketId: string, outgoing: boolean }) {
+import { createClient } from '@/app/lib/supabase/server-client';
+import OutOfStockTicketItemCard from './OutOfStockTicketItemCard';
+import InStockTicketItemCard from './InStockTicketItemCard';
+import UserCard from './UserCard';
+import { User } from '@/app/types/user';
 
+export default async function TicketDetails({
+    ticketId,
+    outgoing,
+}: {
+    ticketId: string;
+    outgoing: boolean;
+}) {
+    const supabase = await createClient();
+
+    const { data: userTicket, error: err } = await supabase
+        .from('tickets')
+        .select(
+            `store_id, ticket_id, requestor_user_id, status, date_submitted,
+      stores (
+        name,
+        street_address
+      )
+    `,
+        )
+        .eq('ticket_id', ticketId)
+        .single();
+
+    if (err) {
+        console.error('Error fetching ticket:', err);
+        return <div>Failed to load data.</div>;
+    }
+
+    let InStockTicketItems: any[] = [];
+    let OutOfStockTicketItems: {
+        ticket_item_id: string;
+        free_text_description: string | null;
+    }[] = [];
+    if (userTicket) {
+        const { data: items } = await supabase
+            .from('ticket_items')
+            .select(
+                `*,
+        store_items(
+          quantity_available,
+          inventory_items(
+            name,
+            photo_url,
+            subcategories(
+              name,
+              categories(
+                name
+              )
+            )
+          )
+        )
+      `,
+            )
+            .eq('ticket_id', ticketId)
+            .eq('is_in_stock_request', true);
+        InStockTicketItems = items || [];
+
+        const { data: outOfStock } = await supabase
+            .from('ticket_items')
+            .select(
+                `ticket_item_id,
+        free_text_description
+      `,
+            )
+            .eq('ticket_id', ticketId)
+            .eq('is_in_stock_request', false);
+        OutOfStockTicketItems = outOfStock || [];
+    }
+
+    const store = userTicket.stores as unknown as {
+        name: string;
+        street_address: string;
+    };
+
+    let storeAdminList: any[] = [];
+    let requestor = null;
+    if (outgoing) {
+        const { data: StoreAdmins } = await supabase
+            .from('store_admins')
+            .select()
+            .eq('store_id', userTicket.store_id);
+        storeAdminList = StoreAdmins || [];
+    } else {
+        if (userTicket) {
+            const { data: Requestor } = await supabase
+                .from('users')
+                .select()
+                .eq('user_id', userTicket.requestor_user_id)
+                .single();
+            requestor = Requestor;
+        }
+    }
+
+    return (
+        <div>
+            {userTicket ? (
+                <div>
+                    <h2>Ticket: {userTicket.ticket_id}</h2>
+                    <h2>Date Submitted: {userTicket.date_submitted}</h2>
+                    <h2>Status: {userTicket.status}</h2>
+                    <h2>Store: {store.name} </h2>
+                    <h2>Store Address: {store.street_address}</h2>
+                    {InStockTicketItems.length > 0 ? (
+                        <div>
+                            <h2> In-Stock Requests</h2>
+                            <div>
+                                {InStockTicketItems.map((item) => (
+                                    <InStockTicketItemCard
+                                        key={item.ticket_item_id}
+                                        ticketItemId={item.ticket_item_id}
+                                        quantityRequested={item.quantity_requested}
+                                        quantityAvailable={
+                                            item.store_items?.quantity_available || 0
+                                        }
+                                        itemName={
+                                            item.store_items?.inventory_items?.name || 'Unknown'
+                                        }
+                                        photoUrl={
+                                            item.store_items?.inventory_items?.photo_url || ''
+                                        }
+                                        subcategoryName={
+                                            item.store_items?.inventory_items?.subcategories?.name ||
+                                            'Unknown'
+                                        }
+                                        categoryName={
+                                            item.store_items?.inventory_items?.subcategories
+                                                ?.categories?.name || 'Unknown'
+                                        }
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    ) : null}
+                    {OutOfStockTicketItems.length > 0 ? (
+                        <div>
+                            <h2>Out-of-Stock Requests</h2>
+                            <div>
+                                {OutOfStockTicketItems.map((item) => (
+                                    <OutOfStockTicketItemCard
+                                        key={item.ticket_item_id}
+                                        ticketItemId={item.ticket_item_id}
+                                        freeTextDescription={
+                                            item.free_text_description || 'No description'
+                                        }
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    ) : null}
+                    {outgoing ? (
+                        <div>
+                            <h2>Contact Store Amdins</h2>
+                            ({storeAdminList.map(storeAdmin => <UserCard user={storeAdmin}></UserCard>)})
+                        </div>
+                    ) : (
+                        <div>
+                            <h2>Contact Requestor</h2>
+                            <UserCard user={requestor}></UserCard>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <p>No ticket found.</p>
+            )}
+        </div>
+    );
 }
--- app/(main)/components/UserCard.tsx
+++ app/(main)/components/UserCard.tsx
@@ -1,10 +1,14 @@
 import { User } from '@/app/types/user';
 
 export default function UserCard({ user }: { user: User }) {
-    return (
-        <div>
-            <h2>{user.first_name} {user.last_name}</h2>
-            <a href={`mailto:${user.email}`}><h3>{user.email}</h3></a>
-        </div>
-    );
+  return (
+    <div>
+      <h2>
+        {user.first_name} {user.last_name}
+      </h2>
+      <a href={`mailto:${user.email}`}>
+        <h3>{user.email}</h3>
+      </a>
+    </div>
+  );
 }
