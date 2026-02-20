3d35c308d0462ca05ef0feb697b32d41d874ec78
neelesh <neeleshseerapu@gmail.com>
2026-02-19 21:50:08 -0800

    Basic User Card Made

---
+++ app/(main)/components/TicketDetails.tsx
@@ -0,0 +1,3 @@
+export default function TicketDetails({ ticketId, outgoing }: { ticketId: string, outgoing: boolean }) {
+
+}
---
+++ app/(main)/components/UserCard.tsx
@@ -0,0 +1,10 @@
+import { User } from '@/app/types/user';
+
+export default function UserCard({ user }: { user: User }) {
+    return (
+        <div>
+            <h2>{user.first_name} {user.last_name}</h2>
+            <a href={`mailto:${user.email}`}><h3>{user.email}</h3></a>
+        </div>
+    );
+}
