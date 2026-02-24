e1833ea7c6b90fdd7c46565777f66390624f0af5
minado05 <athudo@g.ucla.edu>
2026-02-23 22:01:45 -0800

    fixed async error to addStoreItemSearch

--- app/(main)/manage/[storeId]/add/components/AddStoreItemSearch.tsx
+++ app/(main)/manage/[storeId]/add/components/AddStoreItemSearch.tsx
@@ -1,14 +1,21 @@
 'use client';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useFormContext } from
     "react-hook-form";
 import { createClient } from "@/app/lib/supabase/server-client";
 
 export default function AddStoreItemSearch() {
     const { register } = useFormContext()
+    const [results, setResults] = useState([])
     const [searchQuery, setSearchQuery] = useState("")
-    const supabase = createClient()
-    const { data, error } = await supabase.from('inventory_items').select('name').ilike('name', `%${searchQuery}%`)
+
+    useEffect(() => {
+
+        const fetch = async () => {
+            const supabase = await createClient()
+            const { data, error } = await supabase.from('inventory_items').select('name').ilike('name', `%${searchQuery}%`)
+        }
+    }, [])
 
     return <div>
         <input
