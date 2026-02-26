59f3ace8170f27fc54e26950dcd7a8639954b286
joy-y-cheng <joycheng@ucla.edu>
2026-02-26 01:00:38 -0800

    Minor style refactoring

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,14 @@
 import type { ReactNode } from 'react';
+import '@/app/globals.css'
 import Sidebar from '@/app/(main)/components/Sidebar';
 import Breadcrumbs from '@/app/(main)/components/Breadcrumbs';
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div style={{ display: 'flex' }}>
+    <div className='main'>
       {/* Sidebar */}
       <Sidebar />
-
-      <main style={{ flex: 1 }}>
+      <main>
         <Breadcrumbs />
         {/* Main page content */}
         {children}
