b9296ac18ccb08a7cdee674215d3c0c604a4f6da
katieyungchung <katieyungchung@g.ucla.edu>
2026-02-26 22:08:00 -0800

    outgoing tickets, format date to locale string

--- app/(main)/outgoing-tickets/components/OutgoingTicketCard.tsx
+++ app/(main)/outgoing-tickets/components/OutgoingTicketCard.tsx
@@ -3,34 +3,33 @@
 import { usePathname, useRouter } from 'next/navigation';
 import '@/app/globals.css';
 
-type IncomingTicketCardProps = {
-  id: string;
-  requestorFirstName: string;
-  requestorLastName: string;
-  status: string;
+type OutgoingTicketCardProps = {
+  ticketId: string;
+  storeName: string;
   date: string;
+  status: string;
 };
 
 export default function IncomingTicketCard({
-  id,
-  requestorFirstName,
-  requestorLastName,
-  status,
+  ticketId,
+  storeName,
   date,
-}: IncomingTicketCardProps) {
+  status
+  
+}: OutgoingTicketCardProps) {
   const pathname = usePathname();
   const router = useRouter();
 
   // Display requestor's name, status, and date submitted
   return (
     <tr
-      onClick={() => router.push(`${pathname}/${id}`)}
+      onClick={() => router.push(`${pathname}/${ticketId}`)}
       style={{ cursor: 'pointer' }}
     >
-      <td style={{ width: '30%', border: '1px solid #c5c5c5' }}>{id}</td>
+      <td style={{ width: '30%', border: '1px solid #c5c5c5' }}>{ticketId}</td>
+      <td style={{ width: '20%', border: '1px solid #c5c5c5' }}>{storeName}</td>
+      <td style={{ width: '35%', border: '1px solid #c5c5c5' }}>{date}</td>
       <td style={{ width: '20%', border: '1px solid #c5c5c5' }}>{status}</td>
-      <td style={{ width: '35%', border: '1px solid #c5c5c5' }}>{requestorFirstName} {requestorLastName}</td>
-      <td style={{ width: '20%', border: '1px solid #c5c5c5' }}>{new Date(date).toLocaleDateString()}</td>
     </tr>
 
   );
