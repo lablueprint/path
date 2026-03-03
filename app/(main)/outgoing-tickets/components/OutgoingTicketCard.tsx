20e62c543ce5e8676fb0ff35c64224e8754ba9aa
katieyungchung <katieyungchung@g.ucla.edu>
2026-03-02 20:45:24 -0800

    Add work break for overflowing text in table

--- app/(main)/outgoing-tickets/components/OutgoingTicketCard.tsx
+++ app/(main)/outgoing-tickets/components/OutgoingTicketCard.tsx
@@ -19,16 +19,15 @@ export default function OutgoingTicketCard({
   const pathname = usePathname();
   const router = useRouter();
 
-  // Display requestor's name, status, and date submitted
   return (
     <tr
       onClick={() => router.push(`${pathname}/${ticketId}`)}
       style={{ cursor: 'pointer' }}
     >
-      <td style={{ width: '30%', border: '1px solid #c5c5c5' }}>{ticketId}</td>
-      <td style={{ width: '20%', border: '1px solid #c5c5c5' }}>{storeName}</td>
-      <td style={{ width: '20%', border: '1px solid #c5c5c5' }}>{status}</td>
-      <td style={{ width: '35%', border: '1px solid #c5c5c5' }}>{new Date(date).toLocaleString()}</td>
+      <td style={{ width: '30%', border: '1px solid #c5c5c5', wordBreak: 'break-word' }}>{ticketId}</td>
+      <td style={{ width: '20%',border: '1px solid #c5c5c5', wordBreak: 'break-word'}}>{storeName}</td>
+      <td style={{ width: '30%', border: '1px solid #c5c5c5' }}>{status}</td>
+      <td style={{ width: '20%', border: '1px solid #c5c5c5' }}>{new Date(date).toLocaleString()}</td>
     </tr>
   );
 }
