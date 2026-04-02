e20e3c928320583f03c9da854bacbf76e237c6ff
neelesh <neeleshseerapu@gmail.com>
2026-04-01 22:05:02 -0700

    added image to user card

--- app/(main)/components/UserCard.tsx
+++ app/(main)/components/UserCard.tsx
@@ -1,9 +1,11 @@
 import { User } from '@/app/types/user';
 import styles from '@/app/(main)/components/UserCard.module.css';
+import Image from 'next/image';
 
 export default function UserCard({ user }: { user: User }) {
   return (
     <div className={styles['card']}>
+      <Image src={user.profile_photo_url ? user.profile_photo_url : '/default-profile-picture.png'} alt={`Profile picture for ${user.first_name}`} height={100} width={100} />
       <h3>
         {user.first_name} {user.last_name}
       </h3>
