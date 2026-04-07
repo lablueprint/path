ecbfd401a4b638dcbb288ef55608e0b82d3b1634
whsieh12 <williamoda@g.ucla.edu>
2026-04-06 21:47:36 -0700

    Action Buttons

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,12 +1,43 @@
 import Link from 'next/link';
 
-export default function HomePage() {
+import ActionButton from '@/app/(main)/home/components/ActionButton';
+import { createClient } from '@/app/lib/supabase/server-client';
+
+export default async function HomePage() {
+  const supabase = await createClient();
+
+  const { data: claimsData, error: claimsError } =
+    await supabase.auth.getClaims();
+
+  if (claimsError) {
+    console.error('Error fetching claims:', claimsError);
+  }
+
+  const userRole = claimsData?.claims?.user_role;
+
   return (
     <>
       <h1>Home</h1>
-      <Link href="/home/donate">
-        <p>Submit gift-in-kind form</p>
-      </Link>
+
+      <ActionButton
+        text="Submit gift-in-kind form"
+        url="/home/donate"
+      />
+
+      {['requestor', 'admin', 'superadmin', 'owner'].includes(
+        userRole ?? '',
+      ) && (
+        <ActionButton text="Request inventory" url="/request" />
+      )}
+
+      {['admin', 'superadmin', 'owner'].includes(userRole ?? '') && (
+        <ActionButton text="Manage inventory" url="/manage" />
+      )}
+
+      {['superadmin', 'owner'].includes(userRole ?? '') && (
+        <ActionButton text="HQ" url="/hq" />
+      )}
     </>
   );
 }
+
