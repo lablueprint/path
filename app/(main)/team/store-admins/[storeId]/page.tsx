943cf2eab95b59ce334c8b41cdeeab54892f46fb
neelesh <neeleshseerapu@gmail.com>
2026-04-09 22:08:35 -0700

    small change to make copilot happy i guess lol

--- app/(main)/team/store-admins/[storeId]/page.tsx
+++ app/(main)/team/store-admins/[storeId]/page.tsx
@@ -65,7 +65,7 @@ export default async function StoreAdminPage({
           first_name: string;
           last_name: string;
           email: string;
-          profile_photo_url: string;
+          profile_photo_url: string | null;
         };
       }[],
       { merge: false }
