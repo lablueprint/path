02e896c0c4ed5ddd21d81e0a61768d72dd12a0ac
joy-y-cheng <joycheng@ucla.edu>
2026-04-12 22:59:19 -0700

    Format

--- app/(main)/home/components/ActionButton.tsx
+++ app/(main)/home/components/ActionButton.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import styles from './ActionButton.module.css';
 
 type ActionButtonProps = {
-    text: string;
-    url: string;
+  text: string;
+  url: string;
 };
 
 export default function ActionButton({ text, url }: ActionButtonProps) {
   return (
     <Link className={styles['button-text']} href={url}>
-      <div className={styles.button}>{text}</div>
+      <div className={styles['button']}>{text}</div>
     </Link>
   );
 }
