cf3ceca5a057a875e8c633e39425beff23f6dd79
joy-y-cheng <joycheng@ucla.edu>
2026-04-12 22:00:12 -0700

    Update migration files

--- supabase/migrations/20260406013920_handle_ticket_fulfillment.sql
+++ supabase/migrations/20260406013920_handle_ticket_fulfillment.sql
@@ -14,10 +14,6 @@ alter table "public"."tickets" alter column status type "public"."ticket_status"
 
 drop type "public"."ticket_status__old_version_to_be_dropped";
 
-CREATE UNIQUE INDEX uq_requestor_user_id_store_id_draft ON public.tickets USING btree (requestor_user_id, store_id) WHERE (status = 'draft'::public.ticket_status);
-
-CREATE UNIQUE INDEX uq_ticket_id_store_item_id_not_null ON public.ticket_items USING btree (ticket_id, store_item_id) WHERE (store_item_id IS NOT NULL);
-
 set check_function_bodies = off;
 
 CREATE OR REPLACE FUNCTION private.handle_ticket_fulfillment()
@@ -28,22 +24,22 @@ CREATE OR REPLACE FUNCTION private.handle_ticket_fulfillment()
 AS $function$
 begin
 	-- Logic
-  if old.status is distinct from 'fulfilled' 
-    and new.status = 'fulfilled' then
-  update public.store_items store_item
-  set quantity_available = greatest(
-    0,
-    store_item.quantity_available - ticket_item.quantity_requested
-  )
-  from public.ticket_items ticket_item
-  where ticket_item.ticket_id = new.ticket_id
-    and ticket_item.is_in_stock_request = true
-    and ticket_item.store_item_id = store_item.store_item_id
-    and store_item.store_id = new.store_id;
-  
-  end if;
+    if old.status is distinct from 'fulfilled' 
+        and new.status = 'fulfilled' then
+    update public.store_items store_item
+    set quantity_available = greatest(
+        0,
+        store_item.quantity_available - ticket_item.quantity_requested
+    )
+    from public.ticket_items ticket_item
+    where ticket_item.ticket_id = new.ticket_id
+        and ticket_item.is_in_stock_request = true
+        and ticket_item.store_item_id = store_item.store_item_id
+        and store_item.store_id = new.store_id;
+    
+    end if;
 
-  return new;
+    return new;
 end;
 $function$
 ;
@@ -86,5 +82,3 @@ with check ((((( SELECT auth.jwt() AS jwt) ->> 'user_role'::text) = ANY (ARRAY['
 
 
 CREATE TRIGGER "after update tickets status" AFTER UPDATE OF status ON public.tickets FOR EACH ROW EXECUTE FUNCTION private.handle_ticket_fulfillment();
-
-
