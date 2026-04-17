0bd97215e675ebee7e1dbaac314b77da45506ec7
minado05 <athudo@g.ucla.edu>
2026-04-17 00:11:42 -0700

    added toggle grid vs list view

--- app/(main)/components/StoresList.tsx
+++ app/(main)/components/StoresList.tsx
@@ -6,25 +6,61 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import ViewToggle, { ViewMode } from '@/app/(main)/components/ViewToggle';
+import Image from 'next/image';
+import styles from './StoresList.module.css';
 
 export default function StoresList({ stores }: { stores: Store[] }) {
   const [view, setView] = useState<ViewMode>('grid');
 
   return (
     <Container>
-      <ViewToggle onViewChange={(newView) => setView(newView)} />
-      <Row>
-        {stores?.map((store) => (
-          <Col
-            xs={12}
-            sm={view === 'grid' ? 6 : 12}
-            md={view === 'grid' ? 4 : 12}
-            key={store.store_id}
-          >
-            <StoreCard store={store} />
-          </Col>
-        ))}
-      </Row>
+      <div className={styles.header}>
+        <ViewToggle defaultView="grid" onChange={setView} />
+      </div>
+      {view === 'grid' ? (
+        <Row>
+          {stores?.map((store) => (
+            <Col xs={12} sm={6} md={4} key={store.store_id}>
+              <StoreCard store={store} />
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <table className={styles.table}>
+          <thead className={styles.tableHeader}>
+            <tr>
+              <th>NAME</th>
+              <th>ADDRESS</th>
+              {/* whatever columns you need */}
+            </tr>
+          </thead>
+          <tbody>
+            {stores?.map((store) => (
+              <tr className={styles.tableRow} key={store.store_id}>
+                <td>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '8px',
+                    }}
+                  >
+                    <Image
+                      src={'/default-store-photo.png'}
+                      alt={`${store.name} photo`}
+                      width={32}
+                      height={32}
+                      style={{ borderRadius: '50%' }}
+                    />
+                    {store.name}
+                  </div>
+                </td>
+                <td className={styles.address}>{store.street_address}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </Container>
   );
 }
