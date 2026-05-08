ff442ee2d002d4c18131aa65cfc2471a7fc24824
joy-y-cheng <joycheng@ucla.edu>
2026-05-07 22:34:34 -0700

    Minor styling cleanup

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,9 +1,9 @@
 import ActionButton from '@/app/(main)/home/components/ActionButton';
 import { createClient } from '@/app/lib/supabase/server-client';
-import DonateIcon from '@/public/donate.svg';
-import HomeIcon from '@/public/home.svg';
-import InventoryIcon from '@/public/inventory.svg';
-import PieIcon from '@/public/pie.svg';
+import donateIcon from '@/public/donate.svg';
+import homeIcon from '@/public/home.svg';
+import inventoryIcon from '@/public/inventory.svg';
+import pieIcon from '@/public/pie.svg';
 
 export default async function HomePage() {
   const supabase = await createClient();
@@ -22,7 +22,7 @@ export default async function HomePage() {
       <h1>Home</h1>
       <div className="row row-cols-1 row-cols-sm-2 g-5">
         <div className="col">
-          <ActionButton text="Donate" url="/home/donate" icon={DonateIcon} />
+          <ActionButton text="Donate" url="/home/donate" icon={donateIcon} />
         </div>
         <div className="col">
           {['requestor', 'admin', 'superadmin', 'owner'].includes(
@@ -31,7 +31,7 @@ export default async function HomePage() {
             <ActionButton
               text="Request Inventory"
               url="/request"
-              icon={InventoryIcon}
+              icon={inventoryIcon}
             />
           )}
         </div>
@@ -40,13 +40,13 @@ export default async function HomePage() {
             <ActionButton
               text="Manage Inventory"
               url="/manage"
-              icon={HomeIcon}
+              icon={homeIcon}
             />
           )}
         </div>
         <div className="col">
           {['superadmin', 'owner'].includes(userRole ?? '') && (
-            <ActionButton text="HQ" url="/hq" icon={PieIcon} />
+            <ActionButton text="HQ" url="/hq" icon={pieIcon} />
           )}
         </div>
       </div>
