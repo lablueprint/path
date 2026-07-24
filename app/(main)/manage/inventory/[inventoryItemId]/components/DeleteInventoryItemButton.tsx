3e7e6112b086ba1bffb843624e5369b4cd819304
joy-y-cheng <joycheng@ucla.edu>
2026-07-23 21:19:15 -0700

    Delete photo along with inventory item

--- app/(main)/manage/inventory/[inventoryItemId]/components/DeleteInventoryItemButton.tsx
+++ app/(main)/manage/inventory/[inventoryItemId]/components/DeleteInventoryItemButton.tsx
@@ -2,6 +2,7 @@
 
 import { deleteItem } from '@/app/actions/inventory';
 import { useRouter, usePathname } from 'next/navigation';
+import { createClient } from '@/app/lib/supabase/browser-client';
 import { useState, useTransition } from 'react';
 import { Button } from 'react-bootstrap';
 import ConfirmModal from '@/app/(main)/components/ConfirmModal';
@@ -13,6 +14,8 @@ export default function DeleteInventoryItemButton({
 }) {
   const pathname = usePathname();
   const router = useRouter();
+  const supabase = createClient();
+
   const [errorMessage, setErrorMessage] = useState('');
   const [isPending, startTransition] = useTransition();
   const [showModal, setShowModal] = useState(false);
@@ -27,9 +30,13 @@ export default function DeleteInventoryItemButton({
   async function handleDelete() {
     setErrorMessage('');
     startTransition(async () => {
+      // Remove photo from bucket first
+      const { error: storageError } = await supabase.storage
+        .from('inventory_item_photos')
+        .remove([`${inventoryItemId}/item.jpg`]);
       const result = await deleteItem(inventoryItemId);
 
-      if (!result.success) {
+      if (storageError || !result.success) {
         setErrorMessage('Failed to remove item: ' + result.error);
         return;
       }
